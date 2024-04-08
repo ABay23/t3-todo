80e6b68564ae2aa875fb8150c73b1f5bce509b41
ABay23 <alejandrobay23@gmail.com>
2024-04-08 14:39:03 -0400

    Setting up TRPC

---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+
+export const todoRouter = createTRPCRouter({
+  all: protectedProcedure.query(async ({ ctx }) => {
+    const todos = await ctx.db.todo.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+    return [
+      {
+        id: "fake",
+        text: "fake text",
+        completed: false,
+      },
+    ];
+  }),
+});
